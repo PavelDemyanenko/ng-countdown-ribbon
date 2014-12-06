311f8a53ee4a5f9727ca46e273e907d54176c257
xeLL <conscious.suddens@gmail.com>
2014-12-06 17:48:04 +0300

    WIP: Add ng-countdown-ribbon js.

---
+++ src/scripts/ng-countdown-ribbon.js
@@ -0,0 +1,94 @@
+/**
+ * @license ng-countdown-ribbon v0.1.0
+ * http://paveldemyanenko.github.io/ng-countdown-ribbon
+ * (c) 2014 MIT License, http://development.demyanenko.divshot.io
+ */
+(function() {
+    'use strict';
+
+    /**
+     * @description
+     *
+     * This module provides any AngularJS application with a simple countdown robbin.
+     *
+     */
+    var module = angular.module('ngCountdownRibbon', []);
+
+    module.provider('ngCountdownRibbon', function() {
+
+        this.$get = ['$document', '$compile', '$rootScope', '$timeout', '$interval',
+
+            function($document, $compile, $rootScope) {
+
+                var options = {
+                    theme: 'default',
+                    position: 'left',
+                    defaultType: ''
+                };
+
+                var ribbonScope = $rootScope.$new();
+                var tpl = $compile('<span class="ng-countdown-ribbon" ng-class="ngCountdownRibbon.ribbonClass">{{ ngCountdownRibbon.ribbonMessage }}</span>')(ribbonScope);
+
+                $document.find('body').append(tpl);
+
+                var ribbonObject = {
+
+                    config: function(params) {
+                        params = params || {};
+                        angular.extend(options, params);
+                    },
+
+                    set: function(message, type) {
+
+                        if (!message) {
+                            return;
+                        }
+
+                        var ribbonClass = setClass(options.defaultType, type) + ' ' +
+                            setTheme(options.theme) + ' ' +
+                            setPosition(options.position);
+
+                        ribbonScope.ngCountdownRibbon = {
+                            ribbonClass: ribbonClass,
+                            ribbonMessage: message
+                        };
+
+                    }
+                };
+
+                var setClass = function(defaultType, providedType) {
+
+                    var classes = {
+                        defaultClass: 'ng-countdown-ribbon-default',
+                        todayClass: 'ng-countdown-ribbon-today'
+                    };
+
+                    var type = (providedType || defaultType) + 'Class';
+
+                    return classes[type] || classes.defaultClass;
+                };
+
+                var setTheme = function(theme) {
+
+                    var themes = {
+                        default: ''
+                    };
+
+                    return themes[theme] || '';
+                };
+
+                var setPosition = function(position) {
+
+                    var positions = {
+                        left: 'ng-countdown-ribbon-left',
+                        right: 'ng-countdown-ribbon-right'
+                    };
+
+                    return positions[position] || positions.left;
+                };
+
+                return ribbonObject;
+            }
+        ];
+    });
+})();
