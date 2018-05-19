f60f520f6e5cae6acfbb3391b7f433b8a28ede53
PavelDemyanenko <mail@paveldemyanenko.com>
2018-05-19 21:25:33 +0300

    [WIP] Add ctrl spec

--- tests/ng-countdown-ribbon.spec.js
+++ tests/ng-countdown-ribbon.spec.js
@@ -1,45 +1,47 @@
 angular.module('app', ['ngCountdownRibbon'])
-.controller('MainCtrl', ['$scope', 'ngCountdownRibbon',
-    function($scope, ngCountdownRibbon) {
-        var today = new Date();
-        var tomorrow = new Date(today.getTime() + (24 * 60 * 60 * 1000));
+    .controller('MainCtrl', ['$scope', 'ngCountdownRibbon',
+        function($scope, ngCountdownRibbon) {
+            var today = new Date();
+            var tomorrow = new Date(today.getTime() + (24 * 60 * 60 * 1000));
 
-        function formatDate(date) {
-            var d = new Date(date),
-                month = '' + (d.getMonth() + 1),
-                day = '' + d.getDate(),
-                year = d.getFullYear();
-            if (month.length < 2) month = '0' + month;
-            if (day.length < 2) day = '0' + day;
-            return [year, month, day].join('-');
+            function formatDate(date) {
+                var d = new Date(date),
+                    month = '' + (d.getMonth() + 1),
+                    day = '' + d.getDate(),
+                    year = d.getFullYear();
+                if (month.length < 2) month = '0' + month;
+                if (day.length < 2) day = '0' + day;
+                return [year, month, day].join('-');
+            }
+
+            ngCountdownRibbon.set(formatDate(tomorrow), 'http://amzn.com/w/ZWFNUL8AGNLP');
+
+            $scope.getRibbonDate = function() {
+                return '1 day left';
+            }
         }
-
-        ngCountdownRibbon.set(formatDate(tomorrow), 'http://amzn.com/w/ZWFNUL8AGNLP');
-
-        $scope.getRibbonDate = function() {
-            return '1 day left';
-        }
-    }
-]);
+    ]);
 
 
-describe('MainCtrl', function(){
+describe('MainCtrl', function() {
     beforeEach(module('app'));
 
     var $controller, $rootScope;
 
-    beforeEach(inject(function(_$controller_, _$rootScope_){
+    beforeEach(inject(function(_$controller_, _$rootScope_) {
         $controller = _$controller_;
         $rootScope = _$rootScope_;
     }));
 
-  	describe('getRibbonDate()', function(){
-        it('should handle date correctly', inject(function($controller){
+    describe('getRibbonDate()', function() {
+        it('should handle date correctly', inject(function($controller) {
             var $scope = $rootScope.$new();
 
-            var mainCtrl = $controller('MainCtrl', {$scope: $scope});
+            var mainCtrl = $controller('MainCtrl', {
+                $scope: $scope
+            });
 
             $scope.getRibbonDate().should.equal('1 day left');
         }));
-  	});
+    });
 });
