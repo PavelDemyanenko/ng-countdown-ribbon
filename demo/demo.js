8dac1523d01bf0165abdf0701c0d9c85d8ab7386
PavelDemyanenko <mail@paveldemyanenko.com>
2018-05-26 16:42:26 +0300

    Fix jshint:files

--- demo/demo.js
+++ demo/demo.js
@@ -24,8 +24,12 @@ app.controller('MainCtrl', ['$scope', 'ngCountdownRibbon',
                 month = '' + (d.getMonth() + 1),
                 day = '' + d.getDate(),
                 year = d.getFullYear();
-            if (month.length < 2) month = '0' + month;
-            if (day.length < 2) day = '0' + day;
+            if (month.length < 2) {
+              month = '0' + month;
+            }
+            if (day.length < 2) {
+              day = '0' + day;
+            }
             return [year, month, day].join('-');
         }
 
